4197532a0a87187bd558d8ec3e7a1e93368641de
david <dav_S@hotmail.com>
2020-08-16 20:09:20 +0800

    Problem 4, Solution 1

---
+++ Haxe/problem4/Main.hx
@@ -0,0 +1,8 @@
+import src.*;
+
+class Main {
+	public static function main() {
+		var solution = new Solution1();
+		solution.getSolution();
+	}
+}
---
+++ Haxe/problem4/src/Solution1.hx
@@ -0,0 +1,65 @@
+package src;
+
+import haxe.Int64;
+import haxe.Timer;
+
+class Solution1 {
+	public function new() {}
+
+	public function getSolution() {
+		var stamp1 = Timer.stamp();
+		computeSolution();
+		var stamp2 = Timer.stamp();
+		trace('Time taken: ${stamp2 - stamp1}');
+	}
+
+	/**
+	 * Compute the product of 3-digit numbers until the largest palindrome is calculated.
+	 */
+	private function computeSolution() {
+		// Create temporary variable to hold the biggest palindrome
+		var holderNum = 0;
+
+		// Calculate from 100 to 999
+		for (firstNumber in 100...1000) {
+			for (secondNumber in 100...1000) {
+				// For each product, assign the number to the temporary variable if it is palindrome
+				var productNum = firstNumber * secondNumber;
+				if (holderNum < productNum && isPalindrome(productNum)) {
+					holderNum = productNum;
+					trace('$holderNum from $firstNumber * $secondNumber');
+				}
+			}
+		}
+
+		// Trace largest palindrome
+		trace('The largest palindrome from multiplying two 3-digit numbers: $holderNum');
+	}
+
+	/**
+	 * Test the given number to see if it is a palindrome
+	 * @param number
+	 */
+	public function isPalindrome(number:Int64):Bool {
+		// Convert number to String
+		var numString = Std.string(number);
+
+		// First check - test if number given has an even length
+		if (numString.length % 2 != 0) {
+			return false;
+		}
+
+		// Second check - use an array to check if palindrome exists
+		var loopRange = Std.int(numString.length / 2);
+		for (i in 0...loopRange) {
+			// Test if current digit is equal to the corresponding digit from behind
+			var currentDigit = numString.charAt(i);
+			var correspondingDigit = numString.charAt(numString.length - 1 - i);
+			if (currentDigit != correspondingDigit) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
