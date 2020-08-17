cfebbc9e73c100e37d276777b9076da7183ac423
david <dav_S@hotmail.com>
2020-08-17 20:02:10 +0800

    Problem 6, Solution 1

---
+++ Haxe/problem6/Main.hx
@@ -0,0 +1,14 @@
+import haxe.rtti.CType.Abstractdef;
+import src.*;
+
+/**
+ * Find the difference between
+ * the sum of the squares of the first one hundred natural numbers and
+ * the square of the sum.
+ */
+class Main {
+	public static function main() {
+		var AbSolution = new Solution1();
+		AbSolution.getSolution();
+	}
+}
---
+++ Haxe/problem6/src/Solution1.hx
@@ -0,0 +1,55 @@
+package src;
+
+import lib.AbSolution;
+
+class Solution1 extends AbSolution {
+	public function new() {
+		super();
+		trace('test');
+	}
+
+	private override function computeSolution() {
+		var sumOfSquares = sumOfSquares(100);
+		var squareOfSum = squareOfSum(100);
+		trace('The difference is ${squareOfSum - sumOfSquares}');
+	};
+
+	/**
+	 * Compute the sum of all squares from 1 to the given number
+	 * @param upperLimit
+	 */
+	public function sumOfSquares(upperLimit) {
+		// Create temporary variable to hold the sum
+		var numSum = 0;
+
+		// Loop through from 1 to the given number
+		for (i in 1...upperLimit + 1) {
+			// Square the current loop index
+			// Add it to the temporary variable
+			numSum += i * i;
+		}
+
+		// Return the sum
+		return numSum;
+	}
+
+	/**
+	 * Compute the square of the sum from 1 to the given number
+	 * @param upperLimit
+	 */
+	public function squareOfSum(upperLimit) {
+		// Create temporary variable to hold the sum
+		var numSum = 0;
+
+		// Loop through from 1 to the given number
+		for (i in 1...upperLimit + 1) {
+			// Add it to the temporary variable
+			numSum += i;
+		}
+
+		// Square the sum
+		numSum *= numSum;
+		// Return the sum
+		return numSum;
+	}
+}
