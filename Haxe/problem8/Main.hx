0a11eee29f877510d709d397fbf2b273511c4888
david <dav_S@hotmail.com>
2020-08-19 00:17:00 +0800

    Problem 8, Solution 1

---
+++ Haxe/problem8/Main.hx
@@ -0,0 +1,15 @@
+import lib.AbSolution;
+import haxe.rtti.CType.Abstractdef;
+import src.*;
+
+/**
+ * Find the thirteen adjacent digits in the 1000-digit number
+ * that have the greatest product.
+ * What is the value of this product?
+ */
+class Main {
+	public static function main() {
+		var solution1:AbSolution = new Solution1();
+		solution1.getSolution();
+	}
+}
---
+++ Haxe/problem8/src/Solution1.hx
@@ -0,0 +1,64 @@
+package src;
+
+import haxe.Int64;
+import haxe.macro.Expr.QuoteStatus;
+import haxe.Json;
+import sys.io.File;
+import lib.AbSolution;
+
+/**
+ * Brute Force Method
+ */
+class Solution1 extends AbSolution {
+	public function new() {
+		super();
+	}
+
+	var fileContent = '';
+
+	private override function computeSolution() {
+		if (!canLoadFile()) {
+			trace('Unable to load file');
+			return;
+		};
+
+		var adjacentNum = 13;
+		var holderArray = [];
+		var holderNum:Int64 = 1;
+		for (i in 0...fileContent.length) {
+			// Scan through every 13 adjacent numbers
+			// Multiply them,
+			// then compare with old
+			var newNum:Int64 = 1;
+			var newArray = [];
+			for (charNum in i...i + adjacentNum) {
+				var digit = Std.parseInt(fileContent.charAt(charNum));
+				if (digit == 0) {
+					break;
+				}
+
+				newNum *= digit;
+				newArray.push(digit);
+
+				if (newNum > holderNum) {
+					holderNum = newNum;
+					holderArray = newArray;
+				}
+			}
+		}
+
+		trace('The 13 numbers are: $holderArray');
+		trace('The greatest product is $holderNum');
+	}
+
+	/**
+	 * Loads number as string if file system can be accessed.
+	 */
+	private function canLoadFile() {
+		#if sys
+		fileContent = File.getContent("number.txt");
+		return true;
+		#end
+		return false;
+	}
+}
