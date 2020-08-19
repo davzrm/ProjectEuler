dbf8760771931a3b9a2e5c48f9af87c3ebeddad7
david <dav_S@hotmail.com>
2020-08-19 22:52:58 +0800

    Problem 8, Solution 2 optimized+ readable+

--- Haxe/problem8/src/Solution2.hx
+++ Haxe/problem8/src/Solution2.hx
@@ -1,5 +1,6 @@
 package src;
 
+import haxe.macro.Expr.StringLiteralKind;
 import haxe.Int64;
 import sys.io.File;
 import lib.AbSolution;
@@ -20,77 +21,47 @@ class Solution2 extends AbSolution {
 			return;
 		};
 
-		var holderNum:Int64 = 1;
-		var holderArray = [];
+		// Split the long number into arrays at 0's
+		// Remove lengths below 13
+		var adjacentNum = 13;
+		var splitString = fileContent.split('0');
+		splitString = splitString.filter(str -> str.length >= adjacentNum);
+
+		// Create temporary variable to hold largest product & array of the 13 factors
 		var largestProduct:Int64 = 0;
 		var largestArray = [];
+		var holderNum:Int64 = 1;
+		var holderArray = [];
+		// Scan through each split array ---
+		for (string in splitString) {
+			holderNum = 1;
+			holderArray = [];
 
-		var adjacentNum = 13;
-		for (i in 0...adjacentNum) {
-			var digit = Std.parseInt(fileContent.charAt(i));
-			holderNum *= digit;
-			holderArray.push(digit);
-		}
-
-		var index = adjacentNum;
-		while (index < fileContent.length) {
-			// Load new digit & test if 0
-			var newDigit = Std.parseInt(fileContent.charAt(index));
-
-			// Test subsequent 13 digits
-			if (newDigit == 0) {
-				// Reset holder number
-				holderNum = 1;
-				holderArray = [];
-
-				// Set index to the position AFTER the 0
-				index++;
-				var checkPoint = index;
-				var resetSequence = index + adjacentNum;
-
-				while (index < resetSequence) {
-					var nextDigit = Std.parseInt(fileContent.charAt(index));
-
-					// If 0, set index to the latest position of 0
-					// Restart 0 test
-					if (nextDigit == 0) {
-						index++;
-						resetSequence += (index - checkPoint);
-						checkPoint = index;
-						if (resetSequence < fileContent.length) {
-							holderNum = 1;
-							holderArray = [];
-							continue;
-						} else {
-							break;
-						}
-					}
-					holderNum *= nextDigit;
-					holderArray.push(nextDigit);
-					index++;
-				}
-
-				if (largestProduct < holderNum) {
-					largestProduct = holderNum;
-					largestArray = holderArray;
-				}
-				continue;
+			// Multiply the first 13 numbers & compare with the largest product
+			for (charNum in 0...adjacentNum) {
+				var newDigit = Std.parseInt(string.charAt(charNum));
+				holderNum *= newDigit;
+				holderArray.push(newDigit);
 			}
-
-			// Compare -- If new digit is more than old digit
-			// Multiply current product with new digit
-			// Divide current product by old digit
-			var oldDigit = Std.parseInt(fileContent.charAt(index));
-			holderNum = holderNum * newDigit / oldDigit;
-			holderArray[index % adjacentNum] = newDigit;
-
 			if (largestProduct < holderNum) {
 				largestProduct = holderNum;
 				largestArray = holderArray;
 			}
 
-			index++;
+			// Followed by multiplying the next number and divide the last number & compare with largest product
+			for (charNum in adjacentNum...string.length) {
+				var newDigit = Std.parseInt(string.charAt(charNum));
+				var oldDigit = holderArray.shift();
+				holderNum = holderNum * newDigit / oldDigit;
+				holderArray.push(newDigit);
+
+				if (largestProduct < holderNum) {
+					largestProduct = holderNum;
+					largestArray = holderArray;
+				}
+			}
 		}
+
 		trace('The 13 numbers are: $largestArray');
 		trace('The greatest product is $largestProduct');
 	}
